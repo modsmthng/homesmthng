8479aabaf0a8f1a18a2b70af82a3f3a1d4053574
modsmthng <t797fhprsd@privaterelay.appleid.com>
2025-12-10 23:57:11 +0100

    Initial commit of HOMEsmthng

---
+++ HOMEsmthng_v1_0_0/display_setup.h
@@ -0,0 +1,14 @@
+// ** display_setup.h **
+
+#ifndef DISPLAY_SETUP_H
+#define DISPLAY_SETUP_H
+
+#include <LilyGo_RGBPanel.h>
+#include <LV_Helper.h>
+
+extern LilyGo_RGBPanel panel;
+
+// Funktionsdeklaration
+bool setupDisplayAndLVGL(LilyGo_RGBPanel &p);
+
+#endif // DISPLAY_SETUP_H
